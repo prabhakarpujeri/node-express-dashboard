6f76f12fe2effe71adee81409bd69174fa34adf7
Prabhakar pujeri <prabhakar.pujeri@dell.com>
2020-12-08 18:04:04 +0530

    added client code

---
+++ public/javascripts/log-viewer.js
@@ -0,0 +1,11 @@
+const connection = new WebSocket("ws://localhost:3000")
+const logWindow = document.querySelector("#log-window")
+
+connection.onopen = () => {
+	connection.send("Hello from Client")
+
+}
+
+connection.onmessage = (event) => {
+	logWindow.innerHTML = event.data
+}
